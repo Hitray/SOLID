4169537d081cc81a663f954c1059de37aca37c96
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-24 23:47:45 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
 
 
+       
+        
         var purchase = new Purchase();
         purchase.printShowCase();
 
@@ -18,6 +22,7 @@ public class Main {
             purchase.addPurchase(product, count);
         }
 
+       
         purchase.printPurchases();
     }
 }
